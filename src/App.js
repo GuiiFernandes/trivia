4f24de14d8dc66f6aca601caa584c4aecdc738db
Matheus de Souza Cândido <matheus10young@yahoo.com.br>
2023-06-22 16:36:38 -0300

    feat (req-1): router app :rocket:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Game from './pages/Game';
-import Settings from './pages/Settings';
-import Feedback from './pages/Feedback';
-import Ranking from './pages/Ranking';
+// import Login from './pages/Login';
+// import Game from './pages/Game';
+// import Settings from './pages/Settings';
+// import Feedback from './pages/Feedback';
+// import Ranking from './pages/Ranking';
 import './App.css';
 
 export default function App() {
